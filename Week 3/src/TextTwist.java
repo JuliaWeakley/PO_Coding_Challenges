1101f17c8c18b087f8ab90686ea9ed040bdd4f8d
JuliaWeakley <54649800+JuliaWeakley@users.noreply.github.com>
2022-01-19 12:50:03 -0700

    Update TextTwist.java

--- Week 3/src/TextTwist.java
+++ Week 3/src/TextTwist.java
@@ -1,7 +1,9 @@
 
 /*
  * Author: Julia Weakley
- * 
+ * Date: 1/19/2022
+ * Description: players try to score points by forming words using the letters from a 6-letter scrambled word. 
+ * They win the round if they can successfully unscramble the 6-letter word.
  */
 
 import java.util.*; 
@@ -11,17 +13,7 @@ import java.lang.*;
 public class TextTwist 
 {
 	
-	
-/*
- * function that takes in an array of already-guessed words, the unscrambled 6-letter word and returns the total number of points the player scored in a particular round using the following rubric:
-
-3-letter words are 1 pt
-4-letter words are 2 pts
-5-letter words are 3 pts
-6-letter words are 4 pts + 50 pt bonus (for unscrambling the word)
-Remember that invalid words (words that cannot be formed from the 6-letter unscrambled words) count as 0 pts.
- */
-	
+	//function that takes in an array of already-guessed words, the unscrambled 6-letter word and returns the total number of points the player scored
 	static public int totalPoints(String[] te, String word) 
 	{
 		int s =0; 
